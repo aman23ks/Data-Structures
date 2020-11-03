ef5d98d78411bccc61eff10b608e7e5508631556
Aman Shrivastava <nks.ns84@gmail.com>
2020-11-03 18:24:48 +0530

    data structures introduction

---
+++ data-structures-introduction.py
@@ -0,0 +1,26 @@
+# What is a data structure?
+# Data structures is a collection of values, algorithms are the steps or processes we put in place to manipulate these collection of values .
+
+# Data Structures + Alogirthms = Programs
+# A person who knows how to write data structures and algorithms can write programs,good programs.
+
+# Based on different scenarios data needs to be stored in specific formats. We have a handful of data structures that cover are needs.
+
+# How computers store memory
+# In order for a computer to run code it needs to keep track variables such as numbers, strings ,array .
+# These varibales are stored in what we call Random Access Memory(RAM) that how programs run.
+# We have storage(disk drive,flash drive etc.) where the computer stores videos , images ,audio files etc .
+# Data on storage is persistent i.e permanant , when you turn off your computer the data will still be present in the computer
+# Whereas in RAM you loose the memory when the computer is turned off.
+# So you might be wondering why dont we always store data in storage ?
+# The thing is persistent storage is slow.
+# A computer is run by its CPU, we can think of it as a little worker that does all the work inside our computer.
+# And this CPU needs to get access to the RAM and the storage , but it can get access to this RAM much faster.
+
+# Different Operations that can be performed
+# Insertion : Add a new data item to a collection of items.
+# Deletion : Removing data from list
+# Traversal : Access each data item once so it can be accessed.
+# Searching : Finding the location of a data item if it exists in a collection.
+# Sorting : Having data that is sorted.
+# Access : How do we access data in computer.
