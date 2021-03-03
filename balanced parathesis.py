04af92763f9e6bb86ae9efad5efb2dc00f905ad5
Aman Shrivastava <nks.ns84@gmail.com>
2021-03-03 23:09:02 +0530

    balanced parenthesis

--- balanced parathesis.py
+++ balanced parathesis.py
@@ -1,15 +1,14 @@
-class Solution:
-    def isValid(self, s: str) -> bool:
-        list_val = []
-        val = {")": "(", "]": "[", "}": "{"}
-        for i in s:
-            if i in val.values():
-                list_val.append(i)
-            elif i in val.keys() and len(list_val) != 0:
-                if val[i] == list_val[-1]:
-                    list_val.pop()
-                elif val[i] != list_val[-1]:
-                    return False
-            else:
-                return False
-        return len(list_val) == 0
+s = "()[]{}"
+list_val = []
+val = {")": "(", "]": "[", "}": "{"}
+for i in s:
+    if i in val.values():
+        list_val.append(i)
+    elif i in val.keys() and len(list_val) != 0:
+        if val[i] == list_val[-1]:
+            list_val.pop()
+        elif val[i] != list_val[-1]:
+            print(False)
+    else:
+        print(False)
+print(len(list_val) == 0)
