275dd688c094b5980d67b11d118aa635e2d8fed6
Aman Shrivastava <nks.ns84@gmail.com>
2021-04-19 15:26:10 +0530

    median in a row wise sorted matrix

--- median in a row-wise sorted matrix.py
+++ median in a row-wise sorted matrix.py
@@ -2,16 +2,12 @@ matrix = [[1, 3, 5],
           [2, 6, 9],
           [3, 6, 9]]
 
-rowbegin = 0
-rowend = len(matrix)
-columnbegin = 0
-columnend = len(matrix[0])
-res = []
-
-while columnbegin < columnend and rowbegin < rowend:
-    for i in range(rowbegin, rowend):
-        res.append(matrix[i][columnbegin])
-    columnbegin += 1
-
-res = sorted(res)
-print(res[len(res)//2])
+if len(matrix) == 1:
+    vec = matrix[0]
+    print(vec[len(vec)//2])
+else:
+    values = []
+    for row in range(len(matrix)):
+        values.extend(matrix[row])
+    values = sorted(values)
+    print(values[len(values)//2])
