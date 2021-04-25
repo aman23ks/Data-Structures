09f2df0684f388869f84a276eecf47a24e71d06c
Aman Shrivastava <nks.ns84@gmail.com>
2021-04-25 16:48:12 +0530

    sort an array 0's 1's and 2's without using any sorting algorithm

---
+++ sort an array of 0's,1's ad 2's without using any sorting algo.py
@@ -0,0 +1,18 @@
+arr = [0, 2, 1, 2, 0]
+# code here
+low = 0
+mid = 0
+high = len(arr)-1
+
+while mid <= high:
+    if arr[mid] == 0:
+        arr[mid], arr[low] = arr[low], arr[mid]
+        low += 1
+        mid += 1
+    elif arr[mid] == 1:
+        mid += 1
+    elif arr[mid] == 2:
+        arr[mid], arr[high] = arr[high], arr[mid]
+        high = high - 1
+
+print(arr)
