3ffce7349d2bb31a61a6b4968cf1e32840a34804
Aman Shrivastava <nks.ns84@gmail.com>
2021-05-12 01:06:44 +0530

    directed unweighted graph

---
+++ Graphs/directed-unweighted-graph copy.py
@@ -0,0 +1,34 @@
+# Directed weighted graph
+class Graph:
+    def __init__(self):
+        self.adjancency_list = {}
+        self.number_of_nodes = 0
+
+    def add_vertex(self, node1):
+        self.adjancency_list[node1] = []
+        self.number_of_nodes += 1
+
+    def add_edge(self, node1, node2, isDirected=True):
+        if isDirected:
+            self.adjancency_list[node1].append(node2)
+
+    def __str__(self):
+        return f"Adjancency List: {str(self.adjancency_list)}\nNumber of nodes: {self.number_of_nodes}"
+
+
+graph = Graph()
+graph.add_vertex(0)
+graph.add_vertex(1)
+graph.add_vertex(2)
+graph.add_vertex(3)
+graph.add_vertex(4)
+
+graph.add_edge(0, 1, 99)
+graph.add_edge(1, 2, 50)
+graph.add_edge(0, 2, 50)
+graph.add_edge(1, 3, 50)
+graph.add_edge(3, 4, 75)
+graph.add_edge(2, 3, 99)
+graph.add_edge(1, 4, 50)
+
+print(graph)
