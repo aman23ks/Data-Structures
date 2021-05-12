e5e7c0a2b61c87462277916ca97600a6be6ad3aa
Aman Shrivastava <nks.ns84@gmail.com>
2021-05-13 01:33:52 +0530

    detect cycle in a undirected graph

--- detect cycle in a undirected graph.py
+++ detect cycle in a undirected graph.py
@@ -20,7 +20,6 @@ class Graph():
 # Now we will implement the insert node method.
 # We will add the value of the node as a key in the adjacency list and initialize the value of the key to be an empty array
 
-
     def addVertex(self, node):
         self.adjacentList[node] = []
         self.number_of_nodes += 1
@@ -31,14 +30,12 @@ class Graph():
 # If the other node is present it means the edge already exists, if not then the edge doesn't exist.
 # So we add the edge by adding the complimentary node in the adjacency list of either node
 
-
     def addEdge(self, node1, node2):
         self.adjacentList[node1].append(node2)
         self.adjacentList[node2].append(node1)
 
 
 # Finally we will implement a custom print method which willl print the nodes and their connections
-
 
     def __str__(self):
         return str(self.adjacentList)
@@ -68,3 +65,33 @@ myGraph.addEdge(3, 7)
 myGraph.addEdge(4, 5)
 myGraph.addEdge(6, 7)
 print(myGraph)
+
+
+def cycle_exists(G):
+    marked = {u: False for u in G}
+    found_cycle = [False]
+
+    for u in G:
+        if not marked[u]:
+            dfs_visit(G, u, found_cycle, u, marked)
+        if found_cycle[0]:
+            break
+    return found_cycle[0]
+
+
+def dfs_visit(G, u, found_cycle, pred_node, marked):
+    if found_cycle[0]:
+        return
+    marked[u] = True
+    print(marked)
+    for v in G[u]:
+        print(pred_node, v)
+        if marked[v] and v != pred_node:
+
+            found_cycle[0] = True
+            return
+        if not marked[v]:
+            dfs_visit(G, v, found_cycle, u, marked)
+
+
+print(cycle_exists(myGraph.adjacentList))
