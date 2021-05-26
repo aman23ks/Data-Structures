978b9c8c958df8aeff44d2590f65b913f8b50177
Aman Shrivastava <nks.ns84@gmail.com>
2021-05-26 18:39:12 +0530

    common elements in 3 sorted arrays

--- common elements in 3 sorted arrays.py
+++ common elements in 3 sorted arrays.py
@@ -28,25 +28,3 @@ n1 = len(A)
 n2 = len(B)
 n3 = len(C)
 print(commonElements(A, B, C, n1, n2, n3))
-
-'''
-
-if i > 0:
-            if i < n1-1:
-                xx = A[i-1]
-                while A[i] == xx and i < n1-1:
-                    i += 1
-
-        if j > 0:
-            if j < n2-1:
-                yy = A[j-1]
-                while A[j] == yy and j < n2-1:
-                    j += 1
-
-        if k > 0:
-            if k < n3-1:
-                zz = A[k-1]
-                while A[k] == zz and k < n3-1:
-                    k += 1
-
-'''
