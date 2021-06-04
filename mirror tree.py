41fbdb8fe6cbaadc96f11079443aec9aab2f8076
Aman Shrivastava <nks.ns84@gmail.com>
2021-06-05 01:41:02 +0530

    mirror tree

---
+++ mirror tree.py
@@ -0,0 +1,10 @@
+def mirror(self, root):
+
+    if not root:
+        return
+    self.mirror(root.left)
+    self.mirror(root.right)
+
+    root.left, root.right = root.right, root.left
+
+    return root
