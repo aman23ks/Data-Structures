07f153fa30a0c46144c4dd6309a4c887cd903790
Aman Shrivastava <nks.ns84@gmail.com>
2021-06-10 19:08:31 +0530

    sudoku solver

--- rotten oranges.py
+++ rotten oranges.py
@@ -18,20 +18,16 @@ for i in range(row):
 while len(rotten) > 0:
     for i in range(len(rotten)):
         x, y = rotten[0]
-        print(rotten)
         rotten.pop(0)
         lst = [[x-1, y], [x, y-1], [x+1, y], [x, y+1]]
         for r, c in lst:
             if r >= 0 and c >= 0 and r < row and c < column and grid[r][c] == 1:
-                print(fresh)
                 grid[r][c] = 2
                 fresh -= 1
                 rotten.append([r, c])
-                # print(rotten)
     if len(rotten) > 0:
         t += 1
-        print("t", t)
-        print(rotten)
+
 
 if fresh == 0:
     print(t)
