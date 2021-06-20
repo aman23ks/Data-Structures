232a0e3b2daadcaafe8a89a13ba5ee95942e0c2b
Aman Shrivastava <nks.ns84@gmail.com>
2021-06-20 22:53:16 +0530

    minimum swaps required to sort the array

--- minimum number of swaps to sort.py
+++ minimum number of swaps to sort.py
@@ -6,37 +6,14 @@ nums = [7, 16, 14, 17, 6, 9, 5, 3, 18]
 values = []
 count = 0
 
-
-# def check(i, values):
-#     global count
-#     count += 1
-#     var = values[i][1]
-#     temp = values[i]
-#     values[i] = values[var]
-#     values[var] = temp
-
-#     if values[i][1] != i:
-#         check(i, values)
-
-
 for i in range(len(nums)):
     values.append([nums[i], i])
-# print(values[0][1])
-print(values)
+
 values = sorted(values)
-# print(values)
-# for i in range(len(values)-1):
-#     # print(values[[i][1]])
-#     if values[i][1] == i:
-#         # print(values[i])
-#         continue
-#     else:
-#         check(i, values)
-print(values)
+
 i = 0
 while i < len(nums)-1:
     if values[i][1] == i:
-        # print(values[i])
         i += 1
         continue
     else:
@@ -49,5 +26,4 @@ while i < len(nums)-1:
 
 
 print(values)
-# print(values)
 print(count)
