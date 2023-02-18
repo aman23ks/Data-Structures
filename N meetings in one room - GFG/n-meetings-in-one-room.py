b208c08b7c0be88fe78999c956d0fec4e6c206da
Aman Shrivastava <56451181+aman23ks@users.noreply.github.com>
2023-02-18 15:29:56 +0530

    Added solution - LeetHub

--- N meetings in one room - GFG/n-meetings-in-one-room.py
+++ N meetings in one room - GFG/n-meetings-in-one-room.py
@@ -11,14 +11,14 @@ class Solution:
         
         for i in range(len(start)):
             arr.append([end[i],start[i],i])
-        
+            
         arr.sort()
         
         limit = arr[0][0]
         
         count = 1
         
-        for i in range(1,len(arr)):
+        for i in range(1, len(arr)):
             if arr[i][1] > limit:
                 count += 1
                 limit = arr[i][0]
@@ -26,8 +26,8 @@ class Solution:
                 continue
         
         return count
-            
-
+        
+        
 #{ 
  # Driver Code Starts
 #Initial Template for Python 3
