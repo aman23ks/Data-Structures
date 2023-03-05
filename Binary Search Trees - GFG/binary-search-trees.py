f86223999ad16a4f68ddf8a441be3a43a25c4caa
Aman Shrivastava <56451181+aman23ks@users.noreply.github.com>
2023-03-05 15:29:13 +0530

    Added solution - LeetHub

---
+++ Binary Search Trees - GFG/binary-search-trees.py
@@ -0,0 +1,31 @@
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+#Initial Template for Python 3
+
+# } Driver Code Ends
+#User function Template for python3
+
+class Solution:
+    def isBSTTraversal(self, nums):
+        # Code here
+        for i in range(len(nums)-1):
+            if nums[i+1] > nums[i]:
+                continue
+            else:
+                return False
+        
+        return True
+
+#{ 
+ # Driver Code Starts.
+if __name__ == '__main__': 
+    t = int(input ())
+    for _ in range (t):
+        n = int(input())
+        nums = list(map(int, input().split()))
+        ob = Solution()
+        res = ob.isBSTTraversal(nums)
+        print(res)
+# } Driver Code Ends
