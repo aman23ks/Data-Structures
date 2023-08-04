c8989e2fb18fb43b557ea8617c0ba3850b5ba243
Aman Shrivastava <56451181+aman23ks@users.noreply.github.com>
2023-08-05 01:27:48 +0530

    Added solution - LeetHub

--- Knapsack with Duplicate Items - GFG/knapsack-with-duplicate-items.py
+++ Knapsack with Duplicate Items - GFG/knapsack-with-duplicate-items.py
@@ -3,18 +3,22 @@
 class Solution:
     def knapSack(self, N, W, val, wt):
         # code here
-        dp = [[0 for i in range(W+1)] for i in range(N+1)]
-        
-        for i in range(N+1):
+        n = N
+        dp = [[-1 for i in range(W+1)] for i in range(n+1)]
+        # code here
+        for i in range(n+1):
             for j in range(W+1):
                 if i == 0 or j == 0:
                     dp[i][j] = 0
-                if wt[i-1] <= j:
+                
+                elif wt[i-1] <= j:
                     dp[i][j] = max(val[i-1] + dp[i][j-wt[i-1]], dp[i-1][j])
                 else:
-                    dp[i][j] = dp[i-1][j] 
+                    dp[i][j] = dp[i-1][j]
         
-        return dp[N][W]
+        return dp[n][W]
+        
+
 #{ 
  # Driver Code Starts
 #Initial Template for Python 3
