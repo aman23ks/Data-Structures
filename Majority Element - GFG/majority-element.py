02a9e592b95396a037aac2048f410e30538b5bc5
Aman Shrivastava <56451181+aman23ks@users.noreply.github.com>
2023-08-10 07:19:28 +0530

    Added solution - LeetHub

---
+++ Majority Element - GFG/majority-element.py
@@ -0,0 +1,46 @@
+#User function template for Python 3
+
+class Solution:
+    def majorityElement(self, A, N):
+        nums = A
+        hashmap = {}
+        for i in range(len(nums)):
+            if nums[i] not in hashmap:
+                hashmap[nums[i]] = 1
+            else:
+                hashmap[nums[i]] += 1
+        
+        for key, value in hashmap.items():
+            if value > len(nums)//2:
+                return key
+        return -1
+        #Your code here
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import math
+
+from sys import stdin
+
+
+def main():
+        T=int(input())
+        while(T>0):
+            
+            N=int(input())
+
+            A=[int(x) for x in input().strip().split()]
+            
+            
+            obj = Solution()
+            print(obj.majorityElement(A,N))
+            
+            T-=1
+
+
+if __name__ == "__main__":
+    main()
+# } Driver Code Ends
